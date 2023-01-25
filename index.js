f7bd6790369bb28353e3e664d16a54fbd6f43ead
Abubakar <gwabstech@gmail.com>
2023-01-25 12:17:33 +0100

    formated

--- index.js
+++ index.js
@@ -22,10 +22,10 @@ const dataObj = JSON.parse(data);
 
 const slugs = dataObj.map(el => slugify(el.productName, { lower: true }));
 console.log(slugs);
- 
+
 const server = http.createServer((req, res) => {
     const { query, pathname } = url.parse(req.url, true);
-   // console.log(query)
+    // console.log(query)
     // Overview page
     if (pathname === '/' || pathname === '/overview') {
         res.writeHead(200, {
